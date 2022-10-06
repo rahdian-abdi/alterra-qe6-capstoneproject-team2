cec2805f3bec760fb008d706dd0c803922b624c3
rahdian-abdi <rahdianid@gmail.com>
2022-10-06 18:48:28 +0700

    restructured

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -9,6 +9,5 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty"},
         features = "classpath:features"
-        ,tags = "@Users"
 )
 public class CucumberTestSuite {}
